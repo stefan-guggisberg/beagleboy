ec2e0cc7035336f3cf96f9fdf4f6c2470970af6f
Stefan Guggisberg <sguggisb@adobe.com>
2020-04-30 15:50:51 +0200

    ...

--- dispatch/index.js
+++ dispatch/index.js
@@ -12,28 +12,32 @@
 
 const ORIGIN = 'beagleboy--stefan-guggisberg.hlx.page';
 //const ORIGIN = 'origin.beagleboy.org';
+const ORIGIN_MAP = new Map([
+  [ 'beagleboy.org', ORIGIN ],
+  [ 'www.beagleboy.org', ORIGIN ],
+  [ 'stage.beagleboy.org', `stage.${ORIGIN}` ],
+]);
 
 async function fetchResponse(event) {
   const url = new URL(event.request.url);
+  url.hostname = ORIGIN_MAP.get(url.hostname) || ORIGIN;
+/*  
   const cache = caches.default;
   let response = await cache.match(event.request);
 
   if (!response) {
-    response = await fetch(event.request, { cf: { resolveOverride: ORIGIN, cacheTtl: 300 } });  // cache response for 5m
+    //response = await fetch(event.request, { cf: { resolveOverride: ORIGIN, cacheTtl: 300 } });  // cache response for 5m
+    response = await fetch(url.toString(), { cf: { cacheTtl: 300 } });  // cache response for 5m
     const headers = { 'cache-control': 'public, max-age=14400' }; // browser cache ttl: 4h
     response = new Response(response.body, { ...response, headers });
-    event.waitUntil(cache.put(event.request, response.clone()))
+    event.waitUntil(cache.put(event.request, response.clone()));
   }
-  return response
+  return response;
+*/
+  return fetch(url.toString(), { cf: { cacheTtl: 300 } });
 }
 
 async function handleRequest(event) {
-  const url = new URL(event.request.url);
-  if (url.pathname === '/') {
-    url.pathname = '/index.html';
-    return Response.redirect(url.toString(), 301);
-  }
-
   if (event.request.method === 'GET') {
     let response = await fetchResponse(event);
     if (response.status > 399) {
